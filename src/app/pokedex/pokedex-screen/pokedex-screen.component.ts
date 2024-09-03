756956e66c9cce04f4a990ab07590bcfe799d691
Vitali Rudi <159520121+Vitali21MBC@users.noreply.github.com>
2024-09-03 13:12:45 +0200

    Load new pokemons added
    - With a click on the "load pokemon" button, next 30 pokemons get loaded.
    
    -Loading animation added
    
    - Code restructuring

--- src/app/pokedex/pokedex-screen/pokedex-screen.component.ts
+++ src/app/pokedex/pokedex-screen/pokedex-screen.component.ts
@@ -1,25 +1,81 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
-import { PokemonCardSmallComponent } from '../../pokemon-card-small/pokemon-card-small.component';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { PokemonDataService } from '../../pokemon-data.service';
+import { HttpClientModule } from '@angular/common/http';
+import { PokemonCardSmallComponent } from './pokemon-card-small/pokemon-card-small.component';
+import { Subject } from 'rxjs';
 
 @Component({
   selector: 'app-pokedex-screen',
   standalone: true,
-  imports: [CommonModule, PokemonCardSmallComponent],
+  imports: [CommonModule, HttpClientModule, PokemonCardSmallComponent],
   templateUrl: './pokedex-screen.component.html',
   styleUrl: './pokedex-screen.component.scss'
 })
-export class PokedexScreenComponent {
+export class PokedexScreenComponent implements OnInit {
+  @ViewChild(PokemonCardSmallComponent) smallPokemonCards!: PokemonCardSmallComponent;
+
+  isLoading: boolean = false;
+  newPokemonLoaded = new Subject<void>();
 
   constructor(private pokemonDataService: PokemonDataService) { }
 
-  async loadMorePokemon() {
-    console.log("Geklickt");
-    for (let i = 0; i < this.pokemonDataService.getPokemons().length; i++) {
-      // await this.fetchAndProcessPokemon();
-    }
-    // await this.loadPokemonAsBuffer();
+  ngOnInit(): void {
+    this.initialLoadOfPokemon();
   }
 
+  async initialLoadOfPokemon() {
+    for (let i = 2; i < 32; i++) {
+      await this.fetchAndProcessPokemon();
+    }
+  }
+
+  async fetchAndProcessPokemon() {
+    try {
+      const data = await this.pokemonDataService.fetchPokemonOverviewData().toPromise();
+      this.pushPokemonInfoInArray(data);
+    } catch (error) {
+      console.error('Fehler beim Laden der Pokemon-Daten:', error);
+    }
+  }
+
+  pushPokemonInfoInArray(eachPokemon: any) {
+    if (eachPokemon['types'].length === 2) {
+      this.pushTwoTypesInArray(eachPokemon);
+    } else if (eachPokemon['types'].length === 1) {
+      this.pushOneTypeInArray(eachPokemon);
+    }
+  }
+
+  pushTwoTypesInArray(eachPokemon: any) {
+    const pokemonBasicData = {
+      id: eachPokemon['id'],
+      name: eachPokemon['name'],
+      type_1: eachPokemon['types'][0]['type']['name'],
+      type_2: eachPokemon['types'][1]['type']['name'],
+      sprite: eachPokemon['sprites']['front_default'],
+    };
+    this.pokemonDataService.addPokemon(pokemonBasicData);
+  }
+
+  pushOneTypeInArray(eachPokemon: any) {
+    const pokemonBasicData = {
+      id: eachPokemon['id'],
+      name: eachPokemon['name'],
+      type_1: eachPokemon['types'][0]['type']['name'],
+      sprite: eachPokemon['sprites']['front_default'],
+    };
+    this.pokemonDataService.addPokemon(pokemonBasicData);
+  }
+
+  async loadMorePokemon() {
+    this.isLoading = true;
+    for (let i = 2; i < 32; i++) {
+      await this.fetchAndProcessPokemon();
+      this.newPokemonLoaded.next();
+    }
+    this.isLoading = false;
+    console.log('New Pokemon loaded, emitting event'); // Debug log
+
+  }
 }
---
+++ src/app/pokedex/pokedex-screen/pokemon-card-small/pokemon-card-small.component.ts
@@ -0,0 +1,63 @@
+import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
+import { Component, OnInit, OnDestroy, ElementRef } from '@angular/core';
+import { PokemonDataService } from '../../../pokemon-data.service';
+import { PokedexScreenComponent } from '../pokedex-screen.component';
+import { Subscription } from 'rxjs';
+
+@Component({
+  selector: 'app-pokemon-card-small',
+  standalone: true,
+  imports: [CommonModule, HttpClientModule, PokedexScreenComponent],
+  templateUrl: './pokemon-card-small.component.html',
+  styleUrl: './pokemon-card-small.component.scss'
+})
+export class PokemonCardSmallComponent implements OnInit, OnDestroy {
+  private subscription?: Subscription;
+
+  constructor(
+    private pokemonDataService: PokemonDataService,
+    private pokedexScreenComponent: PokedexScreenComponent,
+    private el: ElementRef
+  ) { }
+
+  ngOnInit() {
+    console.log('PokemonCardSmallComponent initialized');
+    this.subscription = this.pokedexScreenComponent.newPokemonLoaded.subscribe(() => {
+      console.log('New Pokemon loaded event received');
+      this.scrollToBottom();
+    });
+  }
+
+  ngOnDestroy() {
+    if (this.subscription) {
+      this.subscription.unsubscribe();
+    }
+  }
+
+  getPokemons() {
+    return this.pokemonDataService.getPokemons();
+  }
+
+  formatPokemonId(id: number): string {
+    if (id < 10) return `#00${id}`;
+    else if (id < 100) return `#0${id}`;
+    else return `#${id}`;
+  }
+
+  backgroundColorAsType(type: string): string {
+    const pokemonTypes = this.pokemonDataService.getPokemonTypes();
+    const matchedType = pokemonTypes.find(t => t.type === type);
+    return matchedType ? matchedType.color : 'gray';
+  }
+
+  private scrollToBottom() {
+    console.log('Attempting to scroll to bottom');
+    setTimeout(() => {
+      const container = this.el.nativeElement;
+      console.log('Scrollable container:', container);
+      console.log('Scrolling to:', container.scrollHeight);
+      container.scrollTop = container.scrollHeight;
+    }, 300);
+  }
+}
